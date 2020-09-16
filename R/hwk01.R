3f4d32cbbc0eeda2f00642f2925b8d4acb844c2a
Aiden Kenny <akenny430@gmail.com>
2020-09-16 01:36:54 -0400

    tested if .gitignore was working

--- R/hwk01.R
+++ R/hwk01.R
@@ -3,11 +3,32 @@ library(ggplot2)
 
 # question 1 --------------------------------------------------------------
 
-# reading in the data
+# reading in the data and creating lowess smoother
 cm <- fread(input = 'data/copier_maintenance.txt')
+cm <- cm[order(minutes), .(copiers, minutes)
+         ][, lowess := lowess(x = cm$copiers, y = cm$minutes)[[2]]]
 
-# plotting the data
+# plotting the data with the lowess smoother
 ggplot(data = cm) +
-  geom_point(mapping = aes(x = minutes, y = copiers), cex = 2) +
-  theme_gray(base_size = 30)
+  geom_point(mapping = aes(x = copiers, y = minutes), cex = 4, pch = 1, stroke = 1.5) +
+  geom_line(mapping = aes(x = copiers, y = lowess), cex = 4, color = 'red') +
+  labs(x = 'Copiers', y = 'Minutes') +
+  theme_minimal(base_size = 40)
 ggsave(filename = 'img/q01_lowess.png')
+
+# fitting the linear regression model
+cm_linear <- lm(minutes ~ copiers, data = cm)
+
+# adding fitted values to the cm data.table
+cm[, fitted := cm_linear$fitted.values]
+
+# plotting the data with the linear regression line
+ggplot(data = cm) +
+  geom_point(mapping = aes(x = copiers, y = minutes), cex = 4, pch = 1, stroke = 1.5) +
+  geom_line(mapping = aes(x = copiers, y = fitted), cex = 4, color = 'orange') +
+  labs(x = 'Copiers', y = 'Minutes') +
+  theme_minimal(base_size = 40)
+ggsave(filename = 'img/q01_linear.png')
+
+# confirming that the residuals sum to 0
+sum(cm_linear$residuals)
