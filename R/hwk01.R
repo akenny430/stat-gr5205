118d77a19a86d43bc907d2cb9edc003cc555c06a
Aiden Kenny <akenny430@gmail.com>
2020-09-21 02:37:56 -0400

    worked on question 4

--- R/hwk01.R
+++ R/hwk01.R
@@ -5,30 +5,89 @@ library(ggplot2)
 
 # reading in the data and creating lowess smoother
 cm <- fread(input = 'data/copier_maintenance.txt')
-cm <- cm[order(minutes), .(copiers, minutes)
-         ][, lowess := lowess(x = cm$copiers, y = cm$minutes)[[2]]]
 
 # plotting the data with the lowess smoother
-ggplot(data = cm) +
-  geom_point(mapping = aes(x = copiers, y = minutes), cex = 4, pch = 1, stroke = 1.5) +
-  geom_line(mapping = aes(x = copiers, y = lowess), cex = 4, color = 'red') +
+ggplot(data = cm, mapping = aes(x = copiers, y = minutes)) +
+  geom_point(cex = 4, pch = 1, stroke = 1.5) +
+  geom_smooth(method = 'loess', color = 'red', cex = 2) +
   labs(x = 'Copiers', y = 'Minutes') +
   theme_minimal(base_size = 40)
 ggsave(filename = 'img/q01_lowess.png')
 
-# fitting the linear regression model
-cm_linear <- lm(minutes ~ copiers, data = cm)
-
-# adding fitted values to the cm data.table
-cm[, fitted := cm_linear$fitted.values]
 
 # plotting the data with the linear regression line
-ggplot(data = cm) +
-  geom_point(mapping = aes(x = copiers, y = minutes), cex = 4, pch = 1, stroke = 1.5) +
-  geom_line(mapping = aes(x = copiers, y = fitted), cex = 4, color = 'orange') +
+ggplot(data = cm, mapping = aes(x = copiers, y = minutes)) +
+  geom_point(cex = 4, pch = 1, stroke = 1.5) +
+  geom_smooth(method = 'lm', color = 'orange', cex = 2) +
   labs(x = 'Copiers', y = 'Minutes') +
   theme_minimal(base_size = 40)
 ggsave(filename = 'img/q01_linear.png')
 
+# fitting the linear regression model
+cm_linear <- lm(minutes ~ copiers, data = cm)
+
 # confirming that the residuals sum to 0
 sum(cm_linear$residuals)
+
+
+# question 2 --------------------------------------------------------------
+
+# reading in data
+sen <- fread(input = 'data/SENIC.txt')
+
+# part (a): loess smoothers
+ggplot(data = sen, mapping = aes(x = Risk, y = Stay)) +
+  geom_point(cex = 4, pch = 1, stroke = 1.5) +
+  geom_smooth(method = 'loess', color = 'red', cex = 2) +
+  labs(x = 'Patient Risk', y = 'Average Stay') +
+  theme_minimal(base_size = 40)
+ggsave(filename = 'hwk/q02_loess1.png')
+
+ggplot(data = sen, mapping = aes(x = AFS, y = Stay)) +
+  geom_point(cex = 4, pch = 1, stroke = 1.5) +
+  geom_smooth(method = 'loess', color = 'red', cex = 2) +
+  labs(x = 'Available Services', y = 'Average Stay') +
+  theme_minimal(base_size = 40)
+ggsave(filename = 'hwk/q02_loess2.png')
+
+ggplot(data = sen, mapping = aes(x = Xray, y = Stay)) +
+  geom_point(cex = 4, pch = 1, stroke = 1.5) +
+  geom_smooth(method = 'loess', color = 'red', cex = 2) +
+  labs(x = 'X-ray Ratio', y = 'Average Stay') +
+  theme_minimal(base_size = 40)
+ggsave(filename = 'hwk/q02_loess3.png')
+
+# part (b): linear model plots
+ggplot(data = sen, mapping = aes(x = Risk, y = Stay)) +
+  geom_point(cex = 4, pch = 1, stroke = 1.5) +
+  geom_smooth(method = 'lm', color = 'orange', cex = 2) +
+  labs(x = 'Patient Risk', y = 'Average Stay') +
+  theme_minimal(base_size = 40)
+ggsave(filename = 'hwk/q02_lm1.png')
+
+ggplot(data = sen, mapping = aes(x = AFS, y = Stay)) +
+  geom_point(cex = 4, pch = 1, stroke = 1.5) +
+  geom_smooth(method = 'lm', color = 'orange', cex = 2) +
+  labs(x = 'Available Services', y = 'Average Stay') +
+  theme_minimal(base_size = 40)
+ggsave(filename = 'hwk/q02_lm2.png')
+
+ggplot(data = sen, mapping = aes(x = Xray, y = Stay)) +
+  geom_point(cex = 4, pch = 1, stroke = 1.5) +
+  geom_smooth(method = 'lm', color = 'orange', cex = 2) +
+  labs(x = 'X-ray Ratio', y = 'Average Stay') +
+  theme_minimal(base_size = 40)
+ggsave(filename = 'hwk/q02_lm3.png')
+
+# fitting the linear models for each to get information
+sen_lm1 <- lm(Stay ~ Risk, data = sen)
+summary(sen_lm1)
+mean(sen_lm1$residuals^2)
+
+sen_lm2 <- lm(Stay ~ AFS, data = sen)
+summary(sen_lm2)
+mean(sen_lm2$residuals^2)
+
+sen_lm3 <- lm(Stay ~ Xray, data = sen)
+summary(sen_lm3)
+mean(sen_lm3$residuals^2)
