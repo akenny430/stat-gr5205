abe7778081d854e30a72b08b315f45f734a57f96
Aiden Kenny <akenny430@gmail.com>
2020-10-05 22:17:12 -0400

    renamed copier data

--- R/hwk01.R
+++ R/hwk01.R
@@ -4,7 +4,7 @@ library(ggplot2)
 # question 1 --------------------------------------------------------------
 
 # reading in the data and creating lowess smoother
-cm <- fread(input = 'data/copier_maintenance.txt')
+cm <- fread(input = 'data/copier-maintenance.txt')
 
 # part (a): plotting the data with the lowess smoother
 ggplot(data = cm, mapping = aes(x = copiers, y = minutes)) +
@@ -12,7 +12,7 @@ ggplot(data = cm, mapping = aes(x = copiers, y = minutes)) +
   geom_smooth(method = 'loess', color = 'red', cex = 2) +
   labs(x = 'Copiers', y = 'Minutes') +
   theme_minimal(base_size = 40)
-ggsave(filename = 'hwk/hwk01/img/q01_lowess.png')
+ggsave(filename = 'hwk/hwk01/img/q01-lowess.png')
 
 # part (b): plotting the data with the linear regression line
 ggplot(data = cm, mapping = aes(x = copiers, y = minutes)) +
@@ -20,7 +20,7 @@ ggplot(data = cm, mapping = aes(x = copiers, y = minutes)) +
   geom_smooth(method = 'lm', color = 'orange', cex = 2) +
   labs(x = 'Copiers', y = 'Minutes') +
   theme_minimal(base_size = 40)
-ggsave(filename = 'hwk/hwk01/img/q01_linear.png')
+ggsave(filename = 'hwk/hwk01/img/q01-linear.png')
 
 # fitting the linear regression model
 cm_linear <- lm(minutes ~ copiers, data = cm)
@@ -51,21 +51,21 @@ ggplot(data = sen, mapping = aes(x = Risk, y = Stay)) +
   geom_smooth(method = 'loess', color = 'red', cex = 2) +
   labs(x = 'Patient Risk', y = 'Average Stay') +
   theme_minimal(base_size = 40)
-ggsave(filename = 'hwk/hwk01/img/q02_loess1.png')
+ggsave(filename = 'hwk/hwk01/img/q02-loess1.png')
 
 ggplot(data = sen, mapping = aes(x = AFS, y = Stay)) +
   geom_point(cex = 4, pch = 1, stroke = 1.5) +
   geom_smooth(method = 'loess', color = 'red', cex = 2) +
   labs(x = 'Available Services', y = 'Average Stay') +
   theme_minimal(base_size = 40)
-ggsave(filename = 'hwk/hwk01/img/q02_loess2.png')
+ggsave(filename = 'hwk/hwk01/img/q02-loess2.png')
 
 ggplot(data = sen, mapping = aes(x = Xray, y = Stay)) +
   geom_point(cex = 4, pch = 1, stroke = 1.5) +
   geom_smooth(method = 'loess', color = 'red', cex = 2) +
   labs(x = 'X-ray Ratio', y = 'Average Stay') +
   theme_minimal(base_size = 40)
-ggsave(filename = 'hwk/hwk01/img/q02_loess3.png')
+ggsave(filename = 'hwk/hwk01/img/q02-loess3.png')
 
 # part (b): linear model plots
 ggplot(data = sen, mapping = aes(x = Risk, y = Stay)) +
@@ -73,21 +73,21 @@ ggplot(data = sen, mapping = aes(x = Risk, y = Stay)) +
   geom_smooth(method = 'lm', color = 'orange', cex = 2) +
   labs(x = 'Patient Risk', y = 'Average Stay') +
   theme_minimal(base_size = 40)
-ggsave(filename = 'hwk/hwk01/img/q02_lm1.png')
+ggsave(filename = 'hwk/hwk01/img/q02-lm1.png')
 
 ggplot(data = sen, mapping = aes(x = AFS, y = Stay)) +
   geom_point(cex = 4, pch = 1, stroke = 1.5) +
   geom_smooth(method = 'lm', color = 'orange', cex = 2) +
   labs(x = 'Available Services', y = 'Average Stay') +
   theme_minimal(base_size = 40)
-ggsave(filename = 'hwk/hwk01/img/q02_lm2.png')
+ggsave(filename = 'hwk/hwk01/img/q02-lm2.png')
 
 ggplot(data = sen, mapping = aes(x = Xray, y = Stay)) +
   geom_point(cex = 4, pch = 1, stroke = 1.5) +
   geom_smooth(method = 'lm', color = 'orange', cex = 2) +
   labs(x = 'X-ray Ratio', y = 'Average Stay') +
   theme_minimal(base_size = 40)
-ggsave(filename = 'hwk/hwk01/img/q02_lm3.png')
+ggsave(filename = 'hwk/hwk01/img/q02-lm3.png')
 
 # fitting the linear models for each to get information
 sen_lm1 <- lm(Stay ~ Risk, data = sen)
