660b91a801363714864b371a3c58f4dabcbc8ed1
Aiden Kenny <akenny430@gmail.com>
2020-11-29 05:19:11 -0500

    added function for normal power transformation

--- R/hwk05-q01.R
+++ R/hwk05-q01.R
@@ -45,16 +45,28 @@ ggplot(senic, aes(x = AFS, y = Nurses)) +
   theme_bw(base_size = 30)
 ggsave("hwk/hwk05/img/q01-scatterplot-blank.png")
 
-ggplot(senic, aes(x = AFS, y = log(Nurses))) +
-  geom_point(cex = 2) +
-  labs(x = "AFS", y = "log(Nurses)") +
-  theme_bw(base_size = 20)
-
 # general observations: 
 # - Nurses could be gamma distributed
 # - AFS looks normally distributed
 # - looking at scatterplot, maybe log transform?
 # - log transform looks good, it makes histogram more normal and scatterplot looks linear
+
+# functions for power transformation and inverse
+power_trans <- function(y, lambda = 0) {
+  if (lambda == 0) {
+    log(y)
+  } else {
+    y^lambda
+  }
+}
+
+power_inv <- function(y, lambda = 0) {
+  if (lambda == 0) {
+    exp(y)
+  } else {
+    y^(1 / lambda)
+  }
+}
 
 # function for box-cox transform, and inverse
 box_cox_trans <- function(y, lambda = 0) {
@@ -115,20 +127,60 @@ ggplot(senic, aes(x = box_cox_trans(Nurses, lambda = best_lamb$lambda), y = ..de
   theme_bw(base_size = 30)
 ggsave("hwk/hwk05/img/q01-nurses-hist-transformed.png")
 
-# getting coefficients for fitted model 
+# creating function of y (i.e. transforming box_cox_trans(Y) back to Y)
 power_fit <- lm(box_cox_trans(Nurses, lambda = best_lamb$lambda) ~ AFS, data = senic)
 transform_fit <- function(x) {
   l <- best_lamb$lambda
   coef <- power_fit$coefficients
-  box_cox_inv(coef[1] + coef[2] * x, lambda = l)
+  box_cox_inv(
+    coef[1] + coef[2] * x, 
+    lambda = l
+    )
 }
+
+# creating functions for confidence interval lower and upper bounds
+confint_lwr <- function(x) {
+  n <- length(senic$Nurses)
+  l <- best_lamb$lambda
+  coef <- power_fit$coefficients
+  glambda <- box_cox_trans(senic$Nurses, lambda = best_lamb$lambda)
+  slambda <- sum((glambda - power_fit$fitted.values)^2) / (n - 2)
+  tstat <- qt(p = 0.975, df = n - 2)
+  lwr_val <- box_cox_inv(
+    coef[1] + coef[2] * x - (tstat * sqrt(slambda / n)), 
+    lambda = best_lamb$lambda
+    )
+  return(lwr_val)
+}
+
+confint_upr <- function(x) {
+  n <- length(senic$Nurses)
+  l <- best_lamb$lambda
+  coef <- power_fit$coefficients
+  glambda <- box_cox_trans(senic$Nurses, lambda = best_lamb$lambda)
+  slambda <- sum((glambda - power_fit$fitted.values)^2) / (n - 2)
+  tstat <- qt(p = 0.975, df = n - 2)
+  upr_val <- box_cox_inv(
+    coef[1] + coef[2] * x + (tstat * sqrt(slambda / n)), 
+    lambda = best_lamb$lambda
+  )
+  return(upr_val)
+}
+
+senic_ci <- senic[, .(AFS, Nurses)
+                  # ][, "trans_Nurses" := box_cox_trans(Nurses, best_lamb$lambda)
+                  ][, "fitted_Nurses" := box_cox_inv(power_fit$fitted.values, best_lamb$lambda)
+                    ][, c("ci_lwr", "ci_upr") := list(confint_lwr(fitted_Nurses), confint_upr(fitted_Nurses))][]
 
 # making scatterplot with optimal lambda function fit over it
 # want to add confidence intervals to this
 ggplot(senic, aes(x = AFS, y = Nurses)) +
   geom_point(color = dark1, cex = 4, pch = 1, stroke = 2) +
-  # geom_smooth(method = "lm", se = FALSE, color = mypink, lwd = 2) +
+  geom_function(fun = confint_lwr, color = myred, lwd = 1) +
+  geom_function(fun = confint_upr, color = myred, lwd = 1) +
+  # geom_ribbon(aes(ymin = ci_lwr, ymax = ci_upr)) +
   geom_function(fun = transform_fit, color = myred, lwd = 3) +
+  # geom_smooth(method = "lm") +
   labs(x = "AFS", y = "Nurses") +
   theme_bw(base_size = 30)
 ggsave("hwk/hwk05/img/q01-scatterplot-model.png")
