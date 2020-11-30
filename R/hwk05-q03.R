8323877575bda4acaacc7f4ccd6201fa5cf1b254
Aiden Kenny <akenny430@gmail.com>
2020-11-30 15:51:16 -0500

    made correlation matrix

--- R/hwk05-q03.R
+++ R/hwk05-q03.R
@@ -19,4 +19,15 @@ library(ggplot2)
 # reading in data
 jobs <- fread(
   "data/job-proficiency.txt"
-)
+) 
+
+# making boxplots
+ggplot(
+  melt(jobs, measure.vars = c("X1", "X2", "X3", "X4")),
+  aes(x = variable, y = value)
+) +
+  geom_boxplot(fill = dark3, color = dark1, cex = 1, outlier.shape = 1, outlier.size = 2, outlier.stroke = 2) +
+  scale_x_discrete(labels = c("1", "2", "3", "4")) +
+  labs(x = "Test", y = "Score") +
+  theme_bw(base_size = 30)
+ggsave("hwk/hwk05/img/q03-boxplot.png")
