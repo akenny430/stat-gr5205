30482e478b61bb1dd1c9b6ba62d10059e77471cd
Aiden Kenny <akenny430@gmail.com>
2020-11-30 15:01:31 -0500

    read in data

---
+++ R/hwk05-q03.R
@@ -0,0 +1,22 @@
+myred    <- "#ff6666"
+myorange <- "#ffcc00"
+myyellow <- "#ffff00"
+mygreen  <- "#99ff33"
+myblue   <- "#99ccff"
+mypink   <- "#ff99ff"
+dark1   <- "#565657"
+dark2   <- "#808081"
+dark3   <- "#aaaaab"
+library(data.table)
+library(ggplot2)
+
+
+
+# part a ------------------------------------------------------------------
+
+
+
+# reading in data
+jobs <- fread(
+  "data/job-proficiency.txt"
+)
